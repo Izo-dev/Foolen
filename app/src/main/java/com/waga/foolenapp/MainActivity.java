62f5a05503446505c89a75d978e2cbdc38e51aa1
Ismael0450 <diakhabysaala@gmail.com>
2021-06-08 12:27:30 +0000

    add login and register

--- app/src/main/java/com/waga/foolenapp/MainActivity.java
+++ app/src/main/java/com/waga/foolenapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.waga.foolenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialButton mRegister;
+    MaterialButton mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRegister = (MaterialButton) findViewById(R.id.register_button);
+        mLogin = (MaterialButton) findViewById(R.id.login_button);
+    }
+
+    public void starRegister(View view) {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void starLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
