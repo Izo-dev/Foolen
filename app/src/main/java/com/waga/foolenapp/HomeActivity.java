9fb58683a0769f62667dd426496780286abb2094
Ismael0450 <diakhabysaala@gmail.com>
2021-06-23 18:50:29 +0000

    home screen ok

--- app/src/main/java/com/waga/foolenapp/HomeActivity.java
+++ app/src/main/java/com/waga/foolenapp/HomeActivity.java
@@ -3,6 +3,7 @@ package com.waga.foolenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void launchDahalo(View view) {
     }
 }
--- app/src/main/java/com/waga/foolenapp/MainActivity.java
+++ app/src/main/java/com/waga/foolenapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void starLogin(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 }
